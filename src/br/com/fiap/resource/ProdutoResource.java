5a1dfdbe253d8c67a2819f8baf2246609b12d8a2
logonrm <logonrm@fiap.com.br>
2017-03-17 22:32:50 -0300

    Implementação do metodo cadastrar,

--- src/br/com/fiap/resource/ProdutoResource.java
+++ src/br/com/fiap/resource/ProdutoResource.java
@@ -2,11 +2,17 @@ package br.com.fiap.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import br.com.fiap.bo.ProdutoBO;
 import br.com.fiap.to.ProdutoTO;
@@ -29,6 +35,17 @@ public class ProdutoResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<ProdutoTO> listar(){
 		return bo.listar();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response cadastrar(ProdutoTO produto,@Context UriInfo uriInfo){
+		bo.cadastrar(produto);
+		//Construir a url para acessar o produto criado
+		UriBuilder url = UriBuilder.fromPath(uriInfo.getPath());
+		url.path(String.valueOf(produto.getCodigo()));
+		//Status 201 - create
+		return Response.created(url.build()).build(); 
 	}
 	
 }
